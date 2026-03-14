17d7c7981bafd90085be489cb1c7ce16608a5394
mciesielski-hub <m.ciesielski@ab-trans.pl>
2026-03-14 11:50:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ TEMPLATES = {
         'price': DEFAULT_PRICE,
         'via_text': 'Slubice',
         'stops': [
-            {'type': 'ZALADUNEK',   'name': 'Dachser Koninko',    'street': 'Drukarska',             'city': '60-023; Koninko; PL',      'time': '18:30', 'day_offset': 0},
-            {'type': 'ROZLADUNEK', 'name': 'Dachser Radeburg',    'street': 'Thomas-Dachser-Strasse 1','city': '01471; Radeburg; DE',      'time': '02:30', 'day_offset': 1},
-            {'type': 'ROZLADUNEK', 'name': 'Dachser Landsberg',   'street': 'Brehnaer Strasse 4',    'city': '06188; Landsberg; DE',     'time': '05:00', 'day_offset': 1},
-            {'type': 'ZALADUNEK',  'name': 'Dachser Landsberg',   'street': 'Brehnaer Strasse 4',    'city': '06188; Landsberg; DE',     'time': '05:00', 'day_offset': 1},
-            {'type': 'ROZLADUNEK', 'name': 'Dachser Schonefeld',  'street': 'Thomas-Dachser-Allee 2','city': '12529; Schonefeld; DE',    'time': '18:00', 'day_offset': 1},
-            {'type': 'ROZLADUNEK (POWROT)', 'name': 'Dachser Koninko', 'street': 'Drukarska',        'city': '60-023; Koninko; PL',      'time': '',      'day_offset': 2},
+            {'type': 'ZALADUNEK',           'name': 'Dachser Koninko',    'street': 'Drukarska',              'city': '60-023; Koninko; PL',   'time': '18:30', 'day_offset': 0},
+            {'type': 'ROZLADUNEK',          'name': 'Dachser Radeburg',   'street': 'Thomas-Dachser-Str 1',   'city': '01471; Radeburg; DE',   'time': '02:30', 'day_offset': 1},
+            {'type': 'ROZLADUNEK',          'name': 'Dachser Landsberg',  'street': 'Brehnaer Strasse 4',     'city': '06188; Landsberg; DE',  'time': '05:00', 'day_offset': 1},
+            {'type': 'ZALADUNEK',           'name': 'Dachser Landsberg',  'street': 'Brehnaer Strasse 4',     'city': '06188; Landsberg; DE',  'time': '05:00', 'day_offset': 1},
+            {'type': 'ROZLADUNEK',          'name': 'Dachser Schonefeld', 'street': 'Thomas-Dachser-Allee 2', 'city': '12529; Schonefeld; DE', 'time': '18:00', 'day_offset': 1},
+            {'type': 'ROZLADUNEK POWROT',   'name': 'Dachser Koninko',    'street': 'Drukarska',              'city': '60-023; Koninko; PL',   'time': '',      'day_offset': 2},
         ]
     },
     'DSV Eupen': {
@@ -26,9 +26,9 @@ TEMPLATES = {
         'vehicle_reg': 'OKR2CN5',
         'via_text': 'Neunkirchen',
         'stops': [
-            {'type': 'ZALADUNEK',          'name': 'Schenker',               'street': 'Siebeponisweg 9', 'city': '4700; Eupen; BE',           'time': '13:10', 'day_offset': 0},
-            {'type': 'PRZELADUNEK',        'name': 'Schenker Deutschland AG','street': 'Boxbergweg 6',    'city': '66538; Neunkirchen; DE',    'time': '21:10', 'day_offset': 1},
-            {'type': 'ROZLADUNEK (POWROT)','name': 'Schenker',               'street': 'Siebeponisweg 9', 'city': '4700; Eupen; BE',           'time': '13:10', 'day_offset': 2},
+            {'type': 'ZALADUNEK',           'name': 'Schenker',               'street': 'Siebeponisweg 9', 'city': '4700; Eupen; BE',        'time': '13:10', 'day_offset': 0},
+            {'type': 'PRZELADUNEK',         'name': 'Schenker Deutschland AG','street': 'Boxbergweg 6',    'city': '66538; Neunkirchen; DE', 'time': '21:10', 'day_offset': 1},
+            {'type': 'ROZLADUNEK POWROT',   'name': 'Schenker',               'street': 'Siebeponisweg 9', 'city': '4700; Eupen; BE',        'time': '13:10', 'day_offset': 2},
         ]
     },
     'Dachser Mouscron': {
@@ -36,11 +36,11 @@ TEMPLATES = {
         'price': '1626 EUR',
         'via_text': 'Alsdorf / Koln',
         'stops': [
-            {'type': 'ZALADUNEK',   'name': 'Dachser Mouscron', 'street': 'Hansestrasse 52',        'city': '7700; Mouscron; BE',        'time': '21:00', 'day_offset': 0},
-            {'type': 'ROZLADUNEK', 'name': 'Dachser Alsdorf',   'street': 'Thomas-Dachser-Strasse 1','city': '52477; Alsdorf; DE',       'time': '01:30', 'day_offset': 1},
-            {'type': 'ROZLADUNEK', 'name': 'Dachser Koln',      'street': 'Rue du Berger',          'city': '51149; Koln; DE',           'time': '03:00', 'day_offset': 1},
-            {'type': 'ZALADUNEK',  'name': 'Dachser Koln',      'street': 'Rue du Berger',          'city': '51149; Koln; DE',           'time': '19:00', 'day_offset': 1},
-            {'type': 'ROZLADUNEK (POWROT)', 'name': 'Dachser Wissous', 'street': 'Hansestrasse 52', 'city': '91320; Wissous; FR',        'time': '02:30', 'day_offset': 2},
+            {'type': 'ZALADUNEK',          'name': 'Dachser Mouscron', 'street': 'Hansestrasse 52',         'city': '7700; Mouscron; BE',  'time': '21:00', 'day_offset': 0},
+            {'type': 'ROZLADUNEK',         'name': 'Dachser Alsdorf',  'street': 'Thomas-Dachser-Str 1',    'city': '52477; Alsdorf; DE',  'time': '01:30', 'day_offset': 1},
+            {'type': 'ROZLADUNEK',         'name': 'Dachser Koln',     'street': 'Rue du Berger',           'city': '51149; Koln; DE',     'time': '03:00', 'day_offset': 1},
+            {'type': 'ZALADUNEK',          'name': 'Dachser Koln',     'street': 'Rue du Berger',           'city': '51149; Koln; DE',     'time': '19:00', 'day_offset': 1},
+            {'type': 'ROZLADUNEK POWROT',  'name': 'Dachser Wissous',  'street': 'Hansestrasse 52',         'city': '91320; Wissous; FR',  'time': '02:30', 'day_offset': 2},
         ]
     }
 }
@@ -52,32 +52,37 @@ def generate_order(template_name, start_date, day_num):
     t = TEMPLATES[template_name]
     load_date = start_date + timedelta(days=day_num)
     ref = load_date.strftime('%Y-%m-%d')
-
-    vehicle_line = ''
-    if t.get('vehicle_reg'):
-        vehicle_line = f'Nr rejestracyjny: {t["vehicle_reg"]}\n'
-
+    client = t['client']
+    price = t['price']
+    via = t['via_text']
+    vreg = t.get('vehicle_reg', '')
+    vehicle_line = 'Nr rejestracyjny: ' + vreg + chr(10) if vreg else ''
     stops_text = ''
     for i, stop in enumerate(t['stops'], 1):
+        stype = stop['type']
+        sname = stop['name']
+        sstreet = stop['street']
+        scity = stop['city']
+        stime = stop['time']
         stop_date = load_date + timedelta(days=stop['day_offset'])
-        time_str = f'  Godzina: {stop["time"]} ({stop_date.strftime("%d.%m.%Y")})\n' if stop['time'] else ''
-        stops_text += f'''{stop['type']} {i}:
-  Firma: {stop['name']}
-  Ulica: {stop['street']}
-  Miasto: {stop['city']}
-{time_str}
-'''
-
-    content = f'''ZLECENIE TRANSPORTOWE
-=====================
-Szablon: {template_name}
-Data: {load_date.strftime('%d.%m.%Y')}
-Referencja: {ref}
-Klient: {t['client']}
-{vehicle_line}Cena: {t['price']}
-
-{stops_text}TRASA: via {t['via_text']}
-'''
+        sdate = stop_date.strftime('%d.%m.%Y')
+        time_line = '  Godzina: ' + stime + ' (' + sdate + ')' + chr(10) if stime else ''
+        stops_text += stype + ' ' + str(i) + ':' + chr(10)
+        stops_text += '  Firma: ' + sname + chr(10)
+        stops_text += '  Ulica: ' + sstreet + chr(10)
+        stops_text += '  Miasto: ' + scity + chr(10)
+        stops_text += time_line + chr(10)
+    load_str = load_date.strftime('%d.%m.%Y')
+    content = 'ZLECENIE TRANSPORTOWE' + chr(10)
+    content += '=====================' + chr(10)
+    content += 'Szablon: ' + template_name + chr(10)
+    content += 'Data: ' + load_str + chr(10)
+    content += 'Referencja: ' + ref + chr(10)
+    content += 'Klient: ' + client + chr(10)
+    content += vehicle_line
+    content += 'Cena: ' + price + chr(10) + chr(10)
+    content += stops_text
+    content += 'TRASA: via ' + via + chr(10)
     return ref, content
 
 st.title('Generator zlecen transportowych')
@@ -102,22 +107,19 @@ if st.button('Generuj zlecenia'):
             orders.append((ref, content))
             generated += 1
         day_counter += 1
-
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w') as zf:
         for ref, content in orders:
-            zf.writestr(f'zlecenie_{ref}.txt', content)
+            zf.writestr('zlecenie_' + ref + '.txt', content)
     zip_buffer.seek(0)
-
-    st.success(f'Wygenerowano {len(orders)} zlecen!')
+    st.success('Wygenerowano ' + str(len(orders)) + ' zlecen!')
     st.download_button(
         label='Pobierz wszystkie zlecenia (ZIP)',
         data=zip_buffer,
-        file_name=f'zlecenia_{template}_{start}.zip',
+        file_name='zlecenia_' + template + '_' + str(start) + '.zip',
         mime='application/zip'
     )
-
     st.subheader('Podglad zlecen:')
     for ref, content in orders:
-        with st.expander(f'Zlecenie {ref}'):
+        with st.expander('Zlecenie ' + ref):
             st.text(content)
